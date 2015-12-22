dfd3618a0977453b077f64957e791b815244b1c6
Lucas França <lucas.teleinfo@gmail.com>
2015-12-21 22:19:32 -0300

    Primeiro commit

---
+++ application/modules/home/views/includes/home/v_footer.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<script src="<?php echo base_url();?>assets/js/jquery-1.11.3.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/jquery-ui.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/jquery.mask.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/Chart.min.js"></script>
+<script src="<?php echo base_url();?>assets/jasny-bootstrap/js/jasny-bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/main.js"></script>
+</body>
+</html>
---
+++ application/modules/home/views/includes/home/v_header.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title><?php echo $titulo;?></title>
+<link rel="shortcut icon" href="<?php echo base_url()?>public/images/logo2.ico">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css" />
+<link rel="stylesheet" href="<?php echo base_url();?>assets/font-awesome-4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/simple-sidebar.css" />
+<link rel="stylesheet" href="<?php echo base_url();?>assets/jasny-bootstrap/css/jasny-bootstrap.min.css"/>
+<link rel="stylesheet" href="<?php echo base_url();?>assets/css/main.css" />
+</head>
+<body>
